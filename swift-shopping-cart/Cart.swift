8d0446a2010acbc025f782c4a296d939ef6ab32e
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-09-28 10:58:52 -0400

    Completed

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -8,5 +8,60 @@
 
 class Cart {
     
+    var items: [Item]
+    
+    init() {
+        self.items = []
+    }
+
+    func totalPriceInCents() -> Int {
+        var totalCost = 0
+        for item in items {
+            totalCost += item.priceInCents
+        }
+        return totalCost
+    }
+    
+    func add(item fromCart: Item) { items.append(fromCart) }
+        
+    func remove(item fromCart: Item) {
+        
+        if let itemFromCart = items.index(of: fromCart) {
+            items.remove(at: itemFromCart)
+        }
+    }
+    
+    func items(withName name: String) -> [Item] {
+        var itemsWithName = [Item]()
+        for item in items {
+            if item.name == name {
+                itemsWithName.append(item)
+            }
+        }
+        return itemsWithName
+    }
+    
+    func items(withMinPrice price: Int) -> [Item] {
+        var itemsWithPrice = [Item]()
+        for item in items {
+            if item.priceInCents >= price {
+                itemsWithPrice.append(item)
+            }
+        }
+        return itemsWithPrice
+    }
+    
+    func items(withMaxPrice price: Int) -> [Item] {
+        var itemsWithPrice = [Item]()
+        for item in items {
+            if item.priceInCents <= price {
+                itemsWithPrice.append(item)
+            }
+        }
+        return itemsWithPrice
+    }
     
 }
+
+
+
